8951b93e60fe28f988e1be4fd2cc7ae9b70880ee
nguyen tan thuan <nhanvathiepsi@gmail.com>
2020-06-12 22:59:43 +0700

    Lưu bài

---
+++ nguentanthuan_lab456/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(nguentanthuan_lab456.Startup))]
+namespace nguentanthuan_lab456
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
