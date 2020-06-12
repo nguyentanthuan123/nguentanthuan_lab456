5517666573caeb57387558608cdbcf52f8e64600
nguyen tan thuan <nhanvathiepsi@gmail.com>
2020-06-12 23:07:56 +0700

    Lưu lại bài 12/06

---
+++ nguentanthuan_lab456/Migrations/202006121606394_PopulateCategoryTable.cs
@@ -0,0 +1,16 @@
+namespace nguentanthuan_lab456.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateCategoryTable : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
